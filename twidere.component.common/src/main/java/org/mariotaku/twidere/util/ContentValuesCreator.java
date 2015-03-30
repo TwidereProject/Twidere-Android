770bb81c89d96b850b71c2ff83dc2e3a0f005032
Mariotaku Lee <mariotaku.lee@gmail.com>
2015-03-31 00:39:43 +0800

    fixed no favorite count issue

--- twidere.component.common/src/main/java/org/mariotaku/twidere/util/ContentValuesCreator.java
+++ twidere.component.common/src/main/java/org/mariotaku/twidere/util/ContentValuesCreator.java
@@ -345,6 +345,7 @@ public final class ContentValuesCreator implements TwidereConstants {
         values.put(Statuses.TEXT_UNESCAPED, toPlainText(text_html));
         values.put(Statuses.RETWEET_COUNT, status.getRetweetCount());
         values.put(Statuses.REPLY_COUNT, status.getReplyCount());
+        values.put(Statuses.FAVORITE_COUNT, status.getFavoriteCount());
         values.put(Statuses.DESCENDENT_REPLY_COUNT, status.getDescendentReplyCount());
         values.put(Statuses.IN_REPLY_TO_STATUS_ID, status.getInReplyToStatusId());
         values.put(Statuses.IN_REPLY_TO_USER_ID, status.getInReplyToUserId());
