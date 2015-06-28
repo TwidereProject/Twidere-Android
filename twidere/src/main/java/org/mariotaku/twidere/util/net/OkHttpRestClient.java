b0a60d9d9ad43f071543b2b5053aef4bb981f930
Mariotaku Lee <mariotaku.lee@gmail.com>
2015-06-29 03:37:19 +0800

    removed code for debugging

--- twidere/src/main/java/org/mariotaku/twidere/util/net/OkHttpRestClient.java
+++ twidere/src/main/java/org/mariotaku/twidere/util/net/OkHttpRestClient.java
@@ -23,7 +23,6 @@ import android.content.Context;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.util.Pair;
 
 import com.squareup.okhttp.Call;
@@ -44,7 +43,6 @@ import org.mariotaku.restfu.http.RestHttpRequest;
 import org.mariotaku.restfu.http.RestHttpResponse;
 import org.mariotaku.restfu.http.RestQueuedRequest;
 import org.mariotaku.restfu.http.mime.TypedData;
-import org.mariotaku.twidere.Constants;
 import org.mariotaku.twidere.util.DebugModeUtils;
 
 import java.io.IOException;
@@ -72,12 +70,7 @@ public class OkHttpRestClient implements RestHttpClient {
     @Override
     public RestHttpResponse execute(RestHttpRequest restHttpRequest) throws IOException {
         final Call call = newCall(restHttpRequest);
-        try {
-            return new OkRestHttpResponse(call.execute());
-        } catch (IOException e) {
-            Log.w(Constants.LOGTAG, e);
-            throw e;
-        }
+        return new OkRestHttpResponse(call.execute());
     }
 
     private Call newCall(final RestHttpRequest restHttpRequest) {
