7f433ed285a9ff45f8564f70a06594ba9c0b88cd
Mariotaku Lee <mariotaku.lee@gmail.com>
2015-09-01 19:11:45 +0800

    fixed upload media error updated version code

--- twidere.component.common/src/main/java/org/mariotaku/twidere/constant/SharedPreferenceConstants.java
+++ twidere.component.common/src/main/java/org/mariotaku/twidere/constant/SharedPreferenceConstants.java
@@ -307,6 +307,6 @@ public interface SharedPreferenceConstants {
     String KEY_HIDE_CARD_ACTIONS = "hide_card_actions";
     @Preference(type = INT, hasDefault = true, defaultInt = 300)
     String KEY_CACHE_SIZE_LIMIT = "cache_size_limit";
-    @Preference(type = BOOLEAN,hasDefault = true,defaultBoolean = true)
+    @Preference(type = BOOLEAN, hasDefault = true, defaultBoolean = true)
     String KEY_BUG_REPORTS = "bug_reports";
 }
