8abc3304d486bd63efa0c2bc1644d3f065133c2f
Mariotaku Lee <mariotaku.lee@gmail.com>
2015-11-25 21:22:29 +0800

    fixing ci errors

--- scripts/install_android_ndk.sh
+++ scripts/install_android_ndk.sh
@@ -2,9 +2,9 @@
 
 if [ `uname -m` = x86_64 ];
 then
-    wget http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin -O ndk.bin
+    wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin -O ndk.bin
 else
-    wget http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86.bin -O ndk.bin
+    wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86.bin -O ndk.bin
 fi
 
 7z x ndk.bin android-ndk-r10e/build/ android-ndk-r10e/ndk-build android-ndk-r10e/platforms/android-21 > /dev/null
