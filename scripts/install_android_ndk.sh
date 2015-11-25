462f94081520d7d2b2fd2024d5ce3f9c40dd2afb
Mariotaku Lee <mariotaku.lee@gmail.com>
2015-11-25 21:13:11 +0800

    added ndk installation script

---
+++ scripts/install_android_ndk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ `uname -m` = x86_64 ];
+then
+    wget http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin -O ndk.bin
+else
+    wget http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86.bin -O ndk.bin
+fi
+
+7z x ndk.bin android-ndk-r10e/build/ android-ndk-r10e/ndk-build android-ndk-r10e/platforms/android-21 > /dev/null
+export ANDROID_NDK_HOME=`pwd`/android-ndk-r10e
+echo "ndk.dir=$ANDROID_NDK_HOME" >> local.properties
