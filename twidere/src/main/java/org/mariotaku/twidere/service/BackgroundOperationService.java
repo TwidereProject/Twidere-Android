35cb5ffc7b1f4bedb20e9d78050dbd89faa970b0
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-05-29 21:28:42 +0800

    fixed 'tweet send' toast

--- twidere/src/main/java/org/mariotaku/twidere/service/BackgroundOperationService.java
+++ twidere/src/main/java/org/mariotaku/twidere/service/BackgroundOperationService.java
@@ -386,6 +386,7 @@ public class BackgroundOperationService extends IntentService implements Constan
 
                 }
             });
+            task.setResultHandler(this);
             task.setParams(Pair.create(actionType, item));
             mHandler.post(new Runnable() {
                 @Override
