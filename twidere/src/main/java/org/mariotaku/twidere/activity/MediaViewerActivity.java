b21f179bb42d4a693673f76073e32be92c7ef2af
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-06-21 14:48:47 +0800

    fixed NPE

--- twidere/src/main/java/org/mariotaku/twidere/activity/MediaViewerActivity.java
+++ twidere/src/main/java/org/mariotaku/twidere/activity/MediaViewerActivity.java
@@ -586,8 +586,9 @@ public final class MediaViewerActivity extends BaseActivity implements IExtended
         @Override
         public void setUserVisibleHint(boolean isVisibleToUser) {
             super.setUserVisibleHint(isVisibleToUser);
-            if (isVisibleToUser) {
-                getActivity().supportInvalidateOptionsMenu();
+            final FragmentActivity activity = getActivity();
+            if (isVisibleToUser && activity != null) {
+                activity.supportInvalidateOptionsMenu();
             }
         }
 
@@ -1027,7 +1028,10 @@ public final class MediaViewerActivity extends BaseActivity implements IExtended
         public void setUserVisibleHint(boolean isVisibleToUser) {
             super.setUserVisibleHint(isVisibleToUser);
             if (isVisibleToUser) {
-                getActivity().supportInvalidateOptionsMenu();
+                final FragmentActivity activity = getActivity();
+                if (activity != null) {
+                    activity.supportInvalidateOptionsMenu();
+                }
             } else if (mVideoView != null && mVideoView.isPlaying()) {
                 mVideoView.pause();
                 updatePlayerState();
@@ -1281,7 +1285,10 @@ public final class MediaViewerActivity extends BaseActivity implements IExtended
         public void setUserVisibleHint(boolean isVisibleToUser) {
             super.setUserVisibleHint(isVisibleToUser);
             if (isVisibleToUser) {
-                getActivity().supportInvalidateOptionsMenu();
+                final FragmentActivity activity = getActivity();
+                if (activity != null) {
+                    activity.supportInvalidateOptionsMenu();
+                }
             }
         }
     }
