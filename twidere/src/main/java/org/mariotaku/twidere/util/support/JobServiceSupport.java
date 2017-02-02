920f9024ea5ed424b52afa57ec4d1107e48bd3ef
Mariotaku Lee <mariotaku.lee@gmail.com>
2017-02-02 12:32:56 +0800

    updated JobServiceSupport.java license to unlicense

--- twidere/src/main/java/org/mariotaku/twidere/util/support/JobServiceSupport.java
+++ twidere/src/main/java/org/mariotaku/twidere/util/support/JobServiceSupport.java
@@ -1,3 +1,29 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org>
+ */
 package org.mariotaku.twidere.util.support;
 
 import android.annotation.TargetApi;
@@ -9,9 +35,9 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
+ * A fix to https://code.google.com/p/android/issues/detail?id=104302
  * Created by mariotaku on 2017/2/2.
  */
-
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class JobServiceSupport {
     public static boolean handleStopJob(JobParameters params, boolean reschedule) {
@@ -35,7 +61,6 @@ public class JobServiceSupport {
                 acknowledgeStopMessageMethod.invoke(callbackCls, params.getJobId(), reschedule);
                 return true;
             } catch (NullPointerException npe) {
-                // https://code.google.com/p/android/issues/detail?id=104302
                 // Treat as handled
                 return true;
             }
