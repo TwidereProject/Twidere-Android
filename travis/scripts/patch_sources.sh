3bd9c61da84c304de3f856506c1ce2e488b08e08
Mariotaku <mariotaku.lee@gmail.com>
2017-04-26 02:20:47 +0800

    fixed wrong patch condition

--- travis/scripts/patch_sources.sh
+++ travis/scripts/patch_sources.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    patch --dry-run -d twidere < twidere/patches/remove_closed_source_dependencies.patch
-else
     echo "Apply real patch for pull request"
     patch -d twidere < twidere/patches/remove_closed_source_dependencies.patch
+else  
+    patch --dry-run -d twidere < twidere/patches/remove_closed_source_dependencies.patch
 fi
