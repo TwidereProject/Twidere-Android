b07a546a160b08666fb1a5efd8310c6f55d1f14a
Michel Roux <xefir@crystalyx.net>
2018-08-25 14:18:36 +0200

    Supporting only static emoji atm

--- twidere/src/main/kotlin/org/mariotaku/twidere/util/emoji/EmojiMastodonTranslator.kt
+++ twidere/src/main/kotlin/org/mariotaku/twidere/util/emoji/EmojiMastodonTranslator.kt
@@ -5,7 +5,7 @@ import org.mariotaku.microblog.library.mastodon.model.Emoji
 fun addEmojisHtml(text: String, emojis: Array<Emoji>): String {
     var finalText = text
     emojis.map { emoji ->
-        val emojiUrl = emoji.url ?: emoji.staticUrl
+        val emojiUrl = emoji.staticUrl ?: emoji.url
         finalText = finalText.replace(":${emoji.shortcode}:", "<emoji src=\"$emojiUrl\">:${emoji.shortcode}:</emoji>")
     }
     return finalText
