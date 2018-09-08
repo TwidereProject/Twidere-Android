edb06d55e1ac18f93315d7aec85f34cb00cf01e1
Michel Roux <xefir@crystalyx.net>
2018-09-08 13:05:57 +0200

    Fix 404 at launch

--- twidere/src/main/kotlin/org/mariotaku/twidere/task/twitter/GetSavedSearchesTask.kt
+++ twidere/src/main/kotlin/org/mariotaku/twidere/task/twitter/GetSavedSearchesTask.kt
@@ -24,6 +24,7 @@ import org.mariotaku.abstask.library.AbstractTask
 import org.mariotaku.microblog.library.MicroBlogException
 import org.mariotaku.sqliteqb.library.Expression
 import org.mariotaku.twidere.TwidereConstants.LOGTAG
+import org.mariotaku.twidere.TwidereConstants.USER_TYPE_TWITTER_COM
 import org.mariotaku.twidere.model.SingleResponse
 import org.mariotaku.twidere.model.UserKey
 import org.mariotaku.twidere.provider.TwidereDataStore.SavedSearches
@@ -42,17 +43,19 @@ class GetSavedSearchesTask(
     override fun doLongOperation(params: Array<UserKey>): SingleResponse<Unit> {
         val cr = context.contentResolver
         for (accountKey in params) {
-            val twitter = MicroBlogAPIFactory.getInstance(context, accountKey) ?: continue
-            try {
-                val searches = twitter.savedSearches
-                val values = ContentValuesCreator.createSavedSearches(searches,
-                        accountKey)
-                val where = Expression.equalsArgs(SavedSearches.ACCOUNT_KEY)
-                val whereArgs = arrayOf(accountKey.toString())
-                cr.delete(SavedSearches.CONTENT_URI, where.sql, whereArgs)
-                ContentResolverUtils.bulkInsert(cr, SavedSearches.CONTENT_URI, values)
-            } catch (e: MicroBlogException) {
-                DebugLog.w(LOGTAG, tr = e)
+            if (accountKey.host == USER_TYPE_TWITTER_COM) {
+                val twitter = MicroBlogAPIFactory.getInstance(context, accountKey) ?: continue
+                try {
+                    val searches = twitter.savedSearches
+                    val values = ContentValuesCreator.createSavedSearches(searches,
+                            accountKey)
+                    val where = Expression.equalsArgs(SavedSearches.ACCOUNT_KEY)
+                    val whereArgs = arrayOf(accountKey.toString())
+                    cr.delete(SavedSearches.CONTENT_URI, where.sql, whereArgs)
+                    ContentResolverUtils.bulkInsert(cr, SavedSearches.CONTENT_URI, values)
+                } catch (e: MicroBlogException) {
+                    DebugLog.w(LOGTAG, tr = e)
+                }
             }
         }
         return SingleResponse(Unit)
