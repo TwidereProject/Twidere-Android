6d4404bf571ec40eb92bf5dc2fad2954aa3d9efb
Tlaster <tlaster@outlook.com>
2021-03-22 10:54:48 +0800

    Revert "fix crash when sharing file for android 11"
    This reverts commit 205424735b929d9f9451171925db214013fc09e2.

--- twidere/src/main/kotlin/org/mariotaku/twidere/activity/MediaViewerActivity.kt
+++ twidere/src/main/kotlin/org/mariotaku/twidere/activity/MediaViewerActivity.kt
@@ -624,12 +624,8 @@ class MediaViewerActivity : BaseActivity(), IMediaViewerActivity, MediaSwipeClos
                 intent.addFlags(Intent.FLAG_GRANT_PREFIX_URI_PERMISSION)
             }
             activity.processShareIntent(intent)
-            val shareIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                Intent.createChooser(intent, activity.getString(R.string.action_share))
-            } else {
-                Intent.createChooser(intent, activity.getString(R.string.action_share))
-            }
-            activity.startActivityForResult(shareIntent, REQUEST_SHARE_MEDIA)
+            activity.startActivityForResult(Intent.createChooser(intent, activity.getString(R.string.action_share)),
+                    REQUEST_SHARE_MEDIA)
         }
 
         override fun onFileSaveFailed() {
